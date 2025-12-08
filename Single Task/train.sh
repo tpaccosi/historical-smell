90cbfbab96d9e1ca1d3576c54e6e7396da7da0b5
Marijn Koolen <marijn.koolen@gmail.com>
2025-12-08 16:17:32 +0100

    Extend training to other languages
    - Update trainer to work with transformers pre- and post- version 4.46
      (`evaluation_strategy` and `eval_strategy` respectively)
    - Add language and model variables to shell script
    - Add missing dependencies

--- Single Task/train.sh
+++ Single Task/train.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 
 todo="train"
-# todo="hyperparam"
+todo="hyperparam"
+language="dutch"
+
+if [ "$language" == "dutch" ];
+then
+  model="emanjavacas/GysBERT"
+elif [ $language == "english" ];
+then
+  model="emanjavacas/MacBERTh"
+fi
 
 if [ "$todo" == "hyperparam" ];
 then
   echo "Hyperparameter search."
   python3 train.py --hypsearch \
-                                 --lang "english" \
+                                 --lang $language \
                                  --fold 1 \
-                                 --model "emanjavacas/MacBERTh"
+                                 --model $model
 elif [ "$todo" == "train" ];
 then
   echo "Train the model."
   python3 train.py --do_train --do_test \
-                                 --lang "english" \
+                                 --lang $language \
                                  --fold 1 \
                                  --learning_rate 5e-05 \
                                  --train_batch_size 16\
                                  --train_epochs 8 \
-                                 --model "emanjavacas/MacBERTh"
+                                 --model $model
 
 elif [ "$todo" == "test" ];
 then
   echo "Test the model."
   python3 train.py --do_test \
-                                 --lang "english" \
+                                 --lang $language \
                                  --fold 1 \
-                                 --model "emanjavacas/MacBERTh"
+                                 --model $model
 
 fi                            
